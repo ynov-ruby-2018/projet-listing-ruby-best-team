cc0a06531276ff9ada0172e4726a3f4e760cc465
Brice MICHALSKI <brice.michalski@icloud.com>
2018-11-07 10:39:17 +0100

    translate && homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_language
+  def set_language
+    if !session[:language]
+      session[:language] = "en"
+    end
+  end
+
 end
