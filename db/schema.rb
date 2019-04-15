86a7d2ad7466746841a8df6c4b119cdc242abde9
Brice MICHALSKI <brice.michalski@icloud.com>
2019-04-15 14:55:19 +0200

    avancement

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_112631) do
+ActiveRecord::Schema.define(version: 2019_04_15_095740) do
+
+  create_table "listings", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
