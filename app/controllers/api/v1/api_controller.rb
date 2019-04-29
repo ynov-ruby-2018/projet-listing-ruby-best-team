370ab4088ed38f4bbf9eea402c3e77543d180cbe
Brice MICHALSKI <brice.michalski@icloud.com>
2019-04-29 17:36:32 +0200

    return products list

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -7,12 +7,13 @@ class Api::V1::ApiController < Api::ApiController
 
     def index
         user = User.find_by(auth_token: request.headers["token"])
+        products = Product.all
         if user.nil?
-            render json: {success: false, errors: [
+            render json: {success: true, errors: [
             {user: ["please authenticate on /api/v1/auth "]}
             ]}
         else
-            render json: {success: false, user: user} 
+            render json: {success: false, products: products} 
         end 
     end
 
