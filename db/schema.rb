73035a937d40299b315811014421c021d8b1fb1a
Brice MICHALSKI <brice.michalski@icloud.com>
2019-04-29 10:53:41 +0200

    use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,17 @@
 
 ActiveRecord::Schema.define(version: 2019_04_16_083925) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "listings", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -29,8 +40,6 @@ ActiveRecord::Schema.define(version: 2019_04_16_083925) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
     t.string "auth_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
